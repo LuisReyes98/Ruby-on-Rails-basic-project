4c9b981602c6cd30f20cb349ca95641c19107d2a
LuisReyes98 <luisrogelio98@gmail.com>
2018-12-05 14:30:40 -0400

    avances python2

--- webdriver_luis.py
+++ webdriver_luis.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 # Se le da al usuario la posibilidad de eligir si desea llevar la prueba a travez de Alerts o Prints
 print("Prueba de pagina iniciada")
 print("Los mensajes de error del sistema se mostraran por consola")
@@ -15,6 +16,7 @@ driver.get('http://localhost:3000');
 time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
 
 def main():
+	global driver
 	pass
 	openNav()
 	openLogin()
@@ -24,12 +26,39 @@ def main():
 	email_box.submit()
 
 	time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
-
-	if driver.find_element_by_class_name('alert'):
-		if value_type == 1:
-			print("Ocurrio un Error de Logeo")
+	openNav()
+	openLogin()
+	try:		
+		if driver.find_element_by_class_name('alert') != None:
+			print("Ocurrio un Error de Inicio de Sesión")
 			pass
+	except Exception as e:
 		pass
+
+	email_box = driver.find_element_by_id('user_email')
+	email_box.send_keys('admin@admin.com')
+
+	password_box = driver.find_element_by_id('user_password')
+	password_box.send_keys('admin123')
+	password_box.submit()
+
+	time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
+
+	openNav()
+	openLogin()
+	try:		
+		if driver.find_element_by_id('log_out') != None:
+			print("Inicio de sesion exitoso")
+			pass		
+	except Exception as e:
+		pass
+
+	driver.find_element_by_class_name('modal-dialog').send_keys('u\'\ue00c\'');
+	driver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+	time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
+
+	openNav()
+	openThemes()
 
 	print('Termine')
 
@@ -45,7 +74,18 @@ def openLogin():
 	login = driver.find_element_by_id('login')
 	login.click()
 	time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
-	
+
+def openThemes():
+	global driver 
+	themes = driver.find_element_by_id('themes')
+	themes.click()
+	time.sleep(5) # Se espera unos segundos para poder apreciar la pagina	
+
+def openCritics():
+	global driver 
+	critics = driver.find_element_by_id('critics')
+	critics.click()
+	time.sleep(5) # Se espera unos segundos para poder apreciar la pagina	
 
 if __name__ == '__main__':
 	main()
