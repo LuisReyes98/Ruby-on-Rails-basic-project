86abcc01edde2e7ed7acea595994e141c69e2b99
LuisReyes98 <luisrogelio98@gmail.com>
2018-12-05 13:10:56 -0400

    avances python

--- webdriver_luis.py
+++ webdriver_luis.py
@@ -1,18 +1,18 @@
 import time
 from selenium import webdriver
 # Se le da al usuario la posibilidad de eligir si desea llevar la prueba a travez de Alerts o Prints
-print("Bienvenido al modulo para probar su pagina web")
-print("Seleccione si prefiere trabajar con print o con alert de javascript")
-print("1 - Print")
-print("2 - Alert")
+print("Prueba de pagina iniciada")
+print("Seleccione si prefiere mensajes por consola o cuadros de dialogo")
+print("1 - Consola")
+print("2 - Dialogo")
 
 value_type = input("Respuesta: ")
 
 while not(int(value_type) == 1 or int(value_type) == 2):
 	print("Ha seleccionado una opcion invalida, por favor eliga una delas opciones indicadas.")
-	print("Seleccione si prefiere trabajar con print o con alert de javascript")
-	print("1 - Print")
-	print("2 - Alert")
+	print("Seleccione si prefiere mensajes por consola o cuadros de dialogo")
+	print("1 - Consola")
+	print("2 - Dialogo")
 	value_type = input("Respuesta: ")
 	pass
 
@@ -24,9 +24,26 @@ driver.maximize_window()
 driver.get('http://localhost:3000');
 time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
 
-nav_bar = driver.find_element_by_id('slide_nav_bar')
+nav_bar = driver.find_element_by_class_name('bm-burger-button')
 nav_bar.click()
 time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
 
+login = driver.find_element_by_id('login')
+login.click()
+time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
+
+email_box = driver.find_element_by_id('user_email')
+email_box.send_keys('demo@oildemo.com')
+email_box.submit()
+
+time.sleep(5) # Se espera unos segundos para poder apreciar la pagina
+
+if driver.find_element_by_class_name('alert'):
+	if value_type == 1:
+		print("Ocurrio un Error de Logeo")
+		pass
+	pass
+
+print('Termine')
 
 
